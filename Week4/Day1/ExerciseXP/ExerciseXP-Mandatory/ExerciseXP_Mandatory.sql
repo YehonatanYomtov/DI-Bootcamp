30f097842e19030df706bfa36c06c1b897556802
Yehonatan <y.yomtov8@gmail.com>
2024-06-23 11:23:51 +0300

    finished exercise xp mandatory of day 1 of week 4

---
+++ Week4/Day1/ExerciseXP/ExerciseXP-Mandatory/ExerciseXP_Mandatory.sql
@@ -0,0 +1,7 @@
+select * from items;
+select * from items where price > 80;
+select * from items where price < 300;
+select * from customers;
+select * from customers where last_name = 'Smith';
+select * from customers where last_name = 'Jones';
+select * from customers where first_name != 'Scott';
