ba111a52f7bb4f88b403987a6951667fcefa31f0
Yehonatan <y.yomtov8@gmail.com>
2024-07-02 18:41:35 +0300

    finished daily challenge words in the stars of day 3 in week 5

---
+++ Week5/Day2/Daily-Challenges/Daily-Challenge-words-in-the-stars/daily_challenge_words_in_the_stars.js
@@ -0,0 +1,31 @@
+// Instructions
+// 1) Prompt the user for several words (separated by commas).
+// 2) Put the words into an array.
+// 3) Console.log the words one per line, in a rectangular frame as seen below.
+// 4) Check out the Hints and Requirements below.
+// For example, if the user gives you:
+// Hello, World, in, a, frame
+// you will transform it to : ["Hello", "World", "in", "a", "frame"]
+// that will get displayed as:
+
+const userInput = "Hello, World, in, a, frame";
+const toArr = userInput.split(", ");
+
+let string = "";
+let longest = 0;
+
+for (let i = 0; i < toArr.length; i++) {
+  if (toArr[i].length > longest) {
+    longest = toArr[i].length;
+  }
+}
+
+const frame = longest + 4;
+
+console.log("*".repeat(frame));
+
+for (let i = 0; i < toArr.length; i++) {
+  console.log("* " + toArr[i] + " ".repeat(longest - toArr[i].length) + " *");
+}
+
+console.log("*".repeat(frame));
