cc600c7581acb917d4be22d367fd54d1b85883de
Yehonatan <y.yomtov8@gmail.com>
2024-07-28 17:19:54 +0300

    changed some functional components to class components in exercise xp mandatory of day 1 in week 9

--- Week9/Day1/Exercise-XP/vite-project/src/UserFavoriteAnimals.jsx
+++ Week9/Day1/Exercise-XP/vite-project/src/UserFavoriteAnimals.jsx
@@ -1,11 +1,17 @@
-function UserFavoriteAnimals({ favAnimals }) {
-  return (
-    <ul>
-      {favAnimals.map((animal, i) => (
-        <li key={i}>{animal}</li>
-      ))}
-    </ul>
-  );
+import { Component } from "react";
+
+class UserFavoriteAnimals extends Component {
+  render() {
+    const { favAnimals } = this.props;
+
+    return (
+      <ul>
+        {favAnimals.map((animal, i) => (
+          <li key={i}>{animal}</li>
+        ))}
+      </ul>
+    );
+  }
 }
 
 export default UserFavoriteAnimals;
